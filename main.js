02c480d7f06ae1900c87d92a475bcb279f808bb0
André Mateus <95716954+Andre-Mateus-Naveca@users.noreply.github.com>
2022-09-01 14:47:30 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
-function preload()
-{
-    classifier = ml5.imageClassifier('Doodlenet')
-}
 function setup()
 {
   canvas = createCanvas(280, 280);
    canvas.center()
     canvas.background("white")
+    canvas.mouseReleased(classifyCanvas);
+}
+function preload()
+{
+    classifier = ml5.imageClassifier('DoodleNet')
 }
 sketch = ""
-array1=['pen','paper','book','bottle']
+array1=['pen','paper','book','bottle','mouse','telephone','clock,','printer']
 randomNumber = Math.floor((Math.random()*array1.length))
 element_of_array = array1[randomNumber]
 console.log(randomNumber)
@@ -37,28 +38,50 @@ function draw()
     // checkSketch()
     // if(drawSketch == sketch)
     // {
-    //     answerHolder = "set"
-    //     score =+ 1;
-    //     document.getElementById("potuacao").innerHTML = "Pontuação: " + score;
-    // }
+        // answerHolder = "set"
+      //  score =+ 1;
+    //  document.getElementById("potuacao").innerHTML = "Pontuação: " + score;
+  //  }
     checkSketch()
+    if(drawSketch == sketch)
+    {
+        answerHolder = "set"
+        score++
+        document.getElementById('score').innerHTML = 'Pontuação: ' + score;
+    }
 }
 
 
  function checkSketch()
  {
-    timerCounter = timerCounter+1
+   
+    timerCounter++
     document.getElementById("tempo").innerHTML = "Tempo: " + timerCounter;
-    console.log(timerCounter);
+    //console.log(timerCounter);
     if(timerCounter > 400)
     {
         timerCounter = 0;
         timerCheck = "completed"
     }
-    if(timerCheck == "completed")
+    if(timerCheck == "completed" || answerHolder == "set")
     {
-        timerCheck = ""
-        answerHolder = ""
-        updateCanvas()
+       timerCheck = ""
+       answerHolder = ""
+       updateCanvas()
+      
     }
+ }
+ function classifyCanvas()
+ {
+    classifier.classify(canvas, gotResult);
+ }
+ function gotResult(error, results)
+ {
+    if(error)
+    {
+        console.error(error)
+    }
+     drawSketch = results[0].label;
+    document.getElementById("esbocoDesenho").innerHTML = 'Nome: ' + drawSketch.replace('_', ' ');
+    document.getElementById("precisao").innerHTML = 'Confidence: ' + Math.round(results[0].confidence * 100) + '%';
  }
