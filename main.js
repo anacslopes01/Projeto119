7233bbf884d7b619283a0dcfe7ddb94cf7ace698
Ana Cristina Silva Lopes <108612731+anacslopes01@users.noreply.github.com>
2022-09-01 16:48:24 -0300

    Comentarios no seu código
    Olá André, verifique os comentários e veja se agora funciona, por favor

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//André, por mais que o código funcione sem o ponto e virgula ; no final dos comandos, é uma boa pratica você coloca-los
+
 function setup()
 {
   canvas = createCanvas(280, 280);
@@ -9,14 +11,14 @@ function preload()
 {
     classifier = ml5.imageClassifier('DoodleNet')
 }
-sketch = ""
-array1=['pen','paper','book','bottle','mouse','telephone','clock,','printer']
-randomNumber = Math.floor((Math.random()*array1.length))
-element_of_array = array1[randomNumber]
-console.log(randomNumber)
-console.log(element_of_array)
-sketch = element_of_array
-document.getElementById("esbocoDesenho").innerHTML = "Esboço a ser Desenhado: " + element_of_array;
+sketch = "";
+array1=['pen','paper','book','bottle','mouse','telephone','clock,','printer'];
+//Primeiro sorteio aqui com math.random e com o array, além de mostrar na tela com innerHTML
+//André, é uma boa somar 1 quando usar Math.random() porque ele sorteia de 0 a 1 (mas não inclue o 1, sorteando 0.1, 0.2... e assim por diante)
+randomNumber = Math.floor((Math.random()*array1.length)+1);
+//melhor usar só a variavel do sketch, pois fica mais organizado seu código
+sketch = array1[randomNumber];
+document.getElementById("esbocoDesenho").innerHTML = "Esboço a ser Desenhado: " + sketch;
 timerCounter = 0;
 timerCheck = "";
 drawSketch = "";
@@ -25,13 +27,13 @@ score = 0;
 function updateCanvas()
 {
     canvas.background("white")
+  //é uma boa sortear de novo para deixar o jogo mais dificil
 }
 function draw()
 {
-    
     strokeWeight(7);
- stroke(0);
- if(mouseIsPressed)
+    stroke(0);
+    if(mouseIsPressed)
 {
     line(pmouseX, pmouseY, mouseX, mouseY)
 }
@@ -42,11 +44,12 @@ function draw()
       //  score =+ 1;
     //  document.getElementById("potuacao").innerHTML = "Pontuação: " + score;
   //  }
-    checkSketch()
+    checkSketch();
+  //agora esse If deve funcionar, pois lá no inicio coloquei o nome sorteado direto na variavel sketch
     if(drawSketch == sketch)
     {
-        answerHolder = "set"
-        score++
+        answerHolder = "set";
+        score++;
         document.getElementById('score').innerHTML = 'Pontuação: ' + score;
     }
 }
@@ -61,13 +64,13 @@ function draw()
     if(timerCounter > 400)
     {
         timerCounter = 0;
-        timerCheck = "completed"
+        timerCheck = "completed";
     }
     if(timerCheck == "completed" || answerHolder == "set")
     {
-       timerCheck = ""
-       answerHolder = ""
-       updateCanvas()
+       timerCheck = "";
+       answerHolder = "";
+       updateCanvas();
       
     }
  }
@@ -80,8 +83,9 @@ function draw()
     if(error)
     {
         console.error(error)
+    } else {  
+        drawSketch = results[0].label;
+        document.getElementById("esbocoDesenho").innerHTML = 'Nome: ' + drawSketch.replace('_', ' ');
+        document.getElementById("precisao").innerHTML = 'Confidence: ' + Math.round(results[0].confidence * 100) + '%';
     }
-     drawSketch = results[0].label;
-    document.getElementById("esbocoDesenho").innerHTML = 'Nome: ' + drawSketch.replace('_', ' ');
-    document.getElementById("precisao").innerHTML = 'Confidence: ' + Math.round(results[0].confidence * 100) + '%';
  }
